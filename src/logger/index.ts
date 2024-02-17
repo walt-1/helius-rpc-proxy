7110720795867340945abb47c29dbe2b15ff652b
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2024-02-16 21:53:14 -0500

    adds url validation to the logger class

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,12 +1,13 @@
 // GFX Logger
 class Logger {
-	public url: string;
+	public url: string | null;
 
 	constructor(url: string) {
-		this.url = url;
+		this.url = this.isValidUrl(url) ? url : null;
 	}
 
-	public async log(data: string): Promise<void> {
+	public async log(data: string): Promise<any> {
+		if (this.url === null) return
 		const options = {
 			method: 'POST',
 			body: JSON.stringify({
@@ -26,7 +27,19 @@ class Logger {
 			console.error(error);
 			return error;
 		}
+	};
+
+	private isValidUrl(url: string) {
+		const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+			'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+		return urlPattern.test(url);
 	}
+
 }
 
 module.exports = Logger;
